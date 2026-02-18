efadb162645c0f047c8ba5119c03228c80744f8a
kekovina <kekovinya@gmail.com>
2026-02-18 18:56:07 +0300

    feat: refactoring

--- src/components/Button/__tests__/Button.e2e-playground.tsx
+++ src/components/Button/__tests__/Button.e2e-playground.tsx
@@ -17,53 +17,24 @@ import {
   type ButtonProps,
 } from '..';
 
-export const combinations: ButtonProps[] = [
-  ...multiCartesian<ButtonProps>([
-    { isLoading: [true, false], isDisabled: [true, false] },
-  ]),
-  ...multiCartesian<ButtonProps>([
-    {
-      before: [IconsMap.CalendarIcon, undefined],
-      after: [IconsMap.CopyIcon, undefined],
-    },
-  ]),
-];
+export const combinations = multiCartesian<ButtonProps>([
+  {
+    isLoading: [true, false],
+    isDisabled: [true, false],
+    theme: [ButtonNeutralTheme, ButtonPrimaryTheme, ButtonSecondaryTheme],
+  },
+  {
+    before: [IconsMap.CalendarIcon, undefined],
+    after: [IconsMap.CopyIcon, undefined],
+    theme: [ButtonNeutralTheme, ButtonPrimaryTheme, ButtonSecondaryTheme],
+  },
+]);
 
-export const ButtonNeutralPlaygroundItem = ({
+export const ButtonPlaygroundItem = ({
   appearance,
   props,
 }: ComponentPlaygroundItemProps<ButtonProps>) => (
   <ComponentPlaygroundItem<ButtonProps> appearance={appearance} props={props}>
-    {(p) => (
-      <Button {...p} theme={ButtonNeutralTheme}>
-        Button
-      </Button>
-    )}
-  </ComponentPlaygroundItem>
-);
-
-export const ButtonPrimaryPlaygroundItem = ({
-  appearance,
-  props,
-}: ComponentPlaygroundItemProps<ButtonProps>) => (
-  <ComponentPlaygroundItem<ButtonProps> appearance={appearance} props={props}>
-    {(p) => (
-      <Button {...p} theme={ButtonPrimaryTheme}>
-        Button
-      </Button>
-    )}
-  </ComponentPlaygroundItem>
-);
-
-export const ButtonSecondaryPlaygroundItem = ({
-  appearance,
-  props,
-}: ComponentPlaygroundItemProps<ButtonProps>) => (
-  <ComponentPlaygroundItem<ButtonProps> appearance={appearance} props={props}>
-    {(p) => (
-      <Button {...p} theme={ButtonSecondaryTheme}>
-        Button
-      </Button>
-    )}
+    {(p) => <Button {...p}>Button</Button>}
   </ComponentPlaygroundItem>
 );
